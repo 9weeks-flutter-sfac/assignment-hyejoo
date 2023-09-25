ccc716ec5de39e76d0d780f4bd0d463ea723fd3a
hedooss <gpwn0408@naver.com>
2023-09-25 22:59:08 +0900

    계산기 수정

--- day4/assignment1/lib/main.dart
+++ day4/assignment1/lib/main.dart
@@ -3,7 +3,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -14,101 +25,99 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     int x = 0;
     int y = 0;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("사칙연산"), centerTitle: true),
-        body: SafeArea(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Row(
-              children: [
-                SizedBox(
-                  width: 15,
-                ),
-                Text("X의 값은?",
-                    style: TextStyle(
-                      fontSize: 18,
-                    )),
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 70),
-                  child: TextField(
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        x = int.parse(value);
-                      },
-                      decoration: InputDecoration(
-                          hintText: "x값을 입력하세요",
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5))))),
-                ))
-              ],
-              mainAxisSize: MainAxisSize.min,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 15,
-                ),
-                Text("Y의 값은?",
-                    style: TextStyle(
-                      fontSize: 18,
-                    )),
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 70),
-                  child: TextField(
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        x = int.parse(value);
-                      },
-                      decoration: InputDecoration(
-                          hintText: "y값을 입력하세요",
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5))))),
-                ))
-              ],
-              mainAxisSize: MainAxisSize.min,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                int result = x + y;
-                showResultDialog(context, result);
-              },
-              child: Text(
-                "더하기의 결과값은?!",
+    return Scaffold(
+      appBar: AppBar(title: Text("사칙연산"), centerTitle: true),
+      body: SafeArea(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 15,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                int result = x * y;
-                showResultDialog(context, result);
-              },
-              child: Text(
-                "곱하기의 결과값은?!",
+              Text("X의 값은?",
+                  style: TextStyle(
+                    fontSize: 18,
+                  )),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 70),
+                child: TextField(
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      x = int.parse(value);
+                    },
+                    decoration: InputDecoration(
+                        hintText: "x값을 입력하세요",
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5))))),
+              ))
+            ],
+            mainAxisSize: MainAxisSize.min,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 15,
               ),
+              Text("Y의 값은?",
+                  style: TextStyle(
+                    fontSize: 18,
+                  )),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 70),
+                child: TextField(
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      y = int.parse(value);
+                    },
+                    decoration: InputDecoration(
+                        hintText: "y값을 입력하세요",
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5))))),
+              ))
+            ],
+            mainAxisSize: MainAxisSize.min,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              int result = x + y;
+              showResultDialog(context, result);
+            },
+            child: Text(
+              "더하기의 결과값은?!",
             ),
-            ElevatedButton(
-              onPressed: () {
-                int result = (x - y).abs();
-                showResultDialog(context, result);
-              },
-              child: Text(
-                "빼기의 결과값은?!",
-              ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              int result = x * y;
+              showResultDialog(context, result);
+            },
+            child: Text(
+              "곱하기의 결과값은?!",
             ),
-            ElevatedButton(
-              onPressed: () {
-                double result = x / y;
-                showResultDialog(context, result);
-              },
-              child: Text(
-                "나누기의 결과값은?!",
-              ),
-            )
-          ]),
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              int result = (x - y).abs();
+              showResultDialog(context, result);
+            },
+            child: Text(
+              "빼기의 결과값은?!",
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              double result = x / y;
+              showResultDialog(context, result);
+            },
+            child: Text(
+              "나누기의 결과값은?!",
+            ),
+          )
+        ]),
       ),
     );
   }
